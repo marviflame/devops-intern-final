de6b28edb05a9341768363276211d78ab14489b7
marviflame <chalcedony800@gmail.com>
2026-08-06 07:13:23 +0000

    posted

--- hello.py
+++ hello.py
@@ -1,20 +1,2 @@
 #!/usr/bin/env python3
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-HOST = "0.0.0.0"
-PORT = 8000
-
-class HelloHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain; charset=utf-8")
-        self.end_headers()
-        self.wfile.write(b"Hello, DevOps!")
-
-    def log_message(self, format, *args):
-        return
-
-if __name__ == "__main__":
-    server = HTTPServer((HOST, PORT), HelloHandler)
-    print(f"Serving on http://{HOST}:{PORT}")
-    server.serve_forever()
+print("Hello, DevOps Baby!")
